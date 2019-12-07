2c7e0e2cd1bd981399e67b3c761d8f380c366190
koldway42 <math12125.jr@gmail.com>
2019-12-07 11:20:19 -0300

    Separação do projeto entre frontend e backend, implementação de serviços

---
+++ backend/CentralErros/Services/ILogService.cs
@@ -0,0 +1,19 @@
+﻿using CentralErros.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CentralErros.Services
+{
+    interface ILogService
+    {
+        Log Save(Log log);
+
+        Log Get(int id);
+
+        List<Log> getAll();
+
+        List<Log> findAllByUserId(int userId);
+    }
+}
